df09cd66f278c110d6da8b893fc3b7b008fad40e
brian <bn3l10@gmail.com>
2022-07-13 09:49:49 -0400

    added grid with rows for time

---
+++ Develop/script.js
@@ -0,0 +1,18 @@
+// create buttons
+// add modal
+// add modal forms
+// create time slots
+// have data or values go into time slot card
+// make time slots editable when you click on time card
+// make shit stay when refreshed and into local storage
+// make it look cool with bootstrap and font
+// maybe add icons to time slot for past due present future and icons for time?
+// add colors to times that are past present future
+// figure out timer so everything is live
+
+var today = moment().format("dddd, MMMM Do YYYY"); 
+console.log(today);
+document.getElementById("today").innerHTML= today;
+
+
+
